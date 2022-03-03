c99c92d72c4e8c16c8b70f8229de3ecf72d48d88
Marimuthu <marimuthu.m@oclocksolutions.com>
2022-03-03 13:00:09 +0530

    Project Setup

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Mari\HellowWorld;
+
+class Index 
+{
+    /**
+     * Print the hellow world string 
+     * 
+     * @param name;
+     * @return string;
+     */
+    public function greeting($greet = "Welcome Hellow world")
+    {
+        return $greet;
+    }
+}
